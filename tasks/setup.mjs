4714214369db8c2d30a6056732e1262327401d63
Opiekun <opiekun.matheus@gmail.com>
2025-06-16 17:18:00 -0300

    setup parallel sync

--- tasks/setup.mjs
+++ tasks/setup.mjs
@@ -2,63 +2,58 @@ import fs from 'fs-extra'
 import path from 'path'
 import log from 'fancy-log'
 import colors from 'ansi-colors'
-
 import { env, projectPath } from '../helpers/config.mjs'
 
-export const setup = callback => {
-  // Create a relative symlink in project root to /vendor/creaminternet/frontools
+export const setup = async (callback) => {
   const relativeDirectory = path.relative(
     projectPath,
-    fs.realpathSync('./')
+    await fs.realpath('./')
   )
   const symlinkDirectoryName = env.symlink || 'tools'
-
-  // Set config files paths
-  const configSamplesPath = './config/'
+  const configSamplesPath = path.resolve('./config/')
   const configPath = path.join(projectPath, 'dev/tools/frontools/config/')
 
-  try {
-    fs.symlinkSync(
-      relativeDirectory,
-      path.join(projectPath, symlinkDirectoryName),
-      'dir'
-    )
+  // Prepare log messages to batch output
+  const logMessages = []
 
-    log(
-      colors.green('Symlink created. You can now use Frontools from the "' + symlinkDirectoryName + '" directory.')
-    )
-  }
-  catch (error) {
-    log(
-      colors.yellow(symlinkDirectoryName + ' already exists. Skipped it.')
-    )
-  }
-
-  // Copy all all non existent config files to /dev/tools/frontools/config/
-  fs.readdirSync(configSamplesPath).forEach((fileName) => {
-    const newFileName = fileName.replace('.sample', '')
-
-    try {
-      fs.copySync(
-        path.join(configSamplesPath, fileName),
-        path.join(configPath, newFileName), {
-          overwrite: false,
-          errorOnExist: true
-        }
-      )
-
-      log('File ' + fileName + ' copied to /dev/tools/frontools/config/' + newFileName)
-    }
-    catch (error) {
-      log(
-        colors.yellow('File ' + newFileName + ' already exists. Skipped it.')
-      )
-    }
+  // Symlink creation task
+  const symlinkTask = fs.symlink(
+    relativeDirectory,
+    path.join(projectPath, symlinkDirectoryName),
+    'dir'
+  ).then(() => {
+    logMessages.push(colors.green(`Symlink created. You can now use Frontools from the "${symlinkDirectoryName}" directory.`))
+  }).catch((error) => {
+    logMessages.push(colors.yellow(`${symlinkDirectoryName} already exists. Skipped it.`))
   })
 
-  log(
-    colors.green('Setup complete! Go to "/dev/tools/frontools/config/" directory and edit the configuration there.')
-  )
+  // Config copy task
+  const configCopyTask = fs.readdir(configSamplesPath)
+    .then(files => Promise.all(files.map(async (fileName) => {
+      const newFileName = fileName.replace('.sample', '')
+      try {
+        await fs.copy(
+          path.join(configSamplesPath, fileName),
+          path.join(configPath, newFileName), {
+            overwrite: false,
+            errorOnExist: true
+          }
+        )
+        logMessages.push(`File ${fileName} copied to /dev/tools/frontools/config/${newFileName}`)
+      } catch (error) {
+        logMessages.push(colors.yellow(`File ${newFileName} already exists. Skipped it.`))
+      }
+    })))
+    .catch(error => {
+      logMessages.push(colors.red('Error reading config samples: ' + error.message))
+    })
 
+  // Run both tasks in parallel
+  await Promise.all([symlinkTask, configCopyTask])
+
+  // Output all log messages at once
+  logMessages.forEach(msg => log(msg))
+
+  log(colors.green('Setup complete! Go to "/dev/tools/frontools/config/" directory and edit the configuration there.'))
   callback()
 }
